49dae1339161bbc35d518ef57dc2d7f84927b8c3
licht1stein <mb@m1k.pw>
2025-06-26 16:46:44 +0100

    Add deployment configuration and remaining files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import solid from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solid()],
+  base: '/',
   server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
   }
 });
